942cea1e61179b55b1c336e5826acf2f24facc3b
Nick Sakellariou <63309852+NickSakellariou@users.noreply.github.com>
2020-12-28 12:22:32 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,14 +1,10 @@
 import operator
 
-
-
 def removeDuplicates(x):
   return list(dict.fromkeys(x))
 
 def reverseList(x):
     print(sorted(x,reverse=True))
-
-
 
 def RemoveDuplicates(x):
     result = {}
@@ -22,7 +18,6 @@ def reverseDict(x):
     print(sorted_a_dict)
 
 
-
 a_list =[10,12,14,14,16,28,28,30]
 
 a_list = removeDuplicates(a_list)
@@ -30,14 +25,9 @@ print(a_list)
 
 reverseList(a_list)
 
-  
-
-  
-
 a_dict={"a":10,"b":12,"c":14,"d":14,"e":16,"f":28,"g":28,"h":30}
 
 a_dict = RemoveDuplicates(a_dict)
 print(a_dict)
 
 reverseDict(a_dict)
-
